79783a2e862324ae686dab00ac9ce773e6c0600f
JulianAG2911 <jaguirre10420@ufide.ac.cr>
2025-02-26 18:55:42 -0600

    quick fix #2

--- Proyecto_Gestion_Proyectos_SolucionesEpsilon/Proyecto_Gestion_Proyectos/RPA.php
+++ Proyecto_Gestion_Proyectos_SolucionesEpsilon/Proyecto_Gestion_Proyectos/RPA.php
@@ -16,7 +16,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seccion de contabilidad</title>
+    <title>Seccion de RPA</title>
     <link rel="stylesheet" href="../CSS/estilos.css">
    
 </head>
