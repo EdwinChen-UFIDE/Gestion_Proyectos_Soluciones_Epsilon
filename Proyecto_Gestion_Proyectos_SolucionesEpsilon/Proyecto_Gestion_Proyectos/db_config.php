5039dfab5a15a9fa677fe409c5446f8ed579fe9c
JulianAG2911 <jaguirre10420@ufide.ac.cr>
2025-03-19 19:28:30 -0600

    s

--- Proyecto_Gestion_Proyectos_SolucionesEpsilon/Proyecto_Gestion_Proyectos/db_config.php
+++ Proyecto_Gestion_Proyectos_SolucionesEpsilon/Proyecto_Gestion_Proyectos/db_config.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = 'localhost';
 $db_name = 'soluci33_soluciones_epsilon';
-$db_user = 'Admin';
+$db_user = 'soluci33_Admin';
 $db_pass = 'u_6%qQW=49~Q'; // Cambiar si tienes contraseña configurada
 
 // Crear conexión
